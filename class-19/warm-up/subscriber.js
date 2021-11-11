6f7078e3b2adf517933e60ff4cd51d20b29e3e94
Evan Woodworth <evan.woodworth@gmail.com>
2021-11-11 14:07:23 -0800

    moved to different location

--- class-19/warm-up/subscriber.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const client = require('socket.io-client');
-const socket = client.connect('http://localhost:3000');
-
-socket.on('publish', (payload) => {
-  console.log(payload);
-  socket.emit('received', payload);
-});
