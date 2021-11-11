988ad031103505e4d61db9d49812495836da7232
Evan Woodworth <evan.woodworth@gmail.com>
2021-11-11 14:04:52 -0800

    warm-up

--- class-19/warm-up/publisher.js
+++ class-19/warm-up/publisher.js
@@ -4,3 +4,7 @@ const client = require('socket.io-client');
 const socket = client.connect('http://localhost:3000');
 
 const input = process.env.argv[2];
+
+socket.emit('publish', {
+  message: input
+})
--- class-19/warm-up/server.js
+++ class-19/warm-up/server.js
@@ -7,9 +7,22 @@ const server = socketio(3000);
 server.on('connection', socket => {
   console.log('Socket connection esablished: ' + socket.id);
 
+  let messageQ = [];
+
   const eventPool = [
     'publish',
     'received',
   ];
 
+  socket.on('publish', (payload) => {
+    messageQ.push(payload.message);
+    server.emit('publish', payload);
+  })
+
+  socket.on('received', (payload) => {
+    const index = messageQ.indexOf(payload.message);
+    if (index > 0) {
+      messageQ.splice(index, 1);
+    }
+  })
 });
